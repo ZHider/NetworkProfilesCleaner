78fedd2d38222e94cb82106b5b8f6e86355831ac
ZHider <littlehider@gmail.com>
2023-12-10 19:33:08 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace NetworkProfilesCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox_2clean = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox_2save = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button_c2s = new System.Windows.Forms.Button();
+            this.button_s2c = new System.Windows.Forms.Button();
+            this.button_swap = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_backup = new System.Windows.Forms.Button();
+            this.button_open = new System.Windows.Forms.Button();
+            this.checkBox_mutiline = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(533, 223);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.listBox_2clean);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 217);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "To Clean";
+            // 
+            // listBox_2clean
+            // 
+            this.listBox_2clean.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_2clean.FormattingEnabled = true;
+            this.listBox_2clean.ItemHeight = 12;
+            this.listBox_2clean.Location = new System.Drawing.Point(3, 17);
+            this.listBox_2clean.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.listBox_2clean.Name = "listBox_2clean";
+            this.listBox_2clean.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox_2clean.Size = new System.Drawing.Size(234, 197);
+            this.listBox_2clean.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.listBox_2save);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(289, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 217);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "To Save";
+            // 
+            // listBox_2save
+            // 
+            this.listBox_2save.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_2save.FormattingEnabled = true;
+            this.listBox_2save.ItemHeight = 12;
+            this.listBox_2save.Location = new System.Drawing.Point(3, 17);
+            this.listBox_2save.Name = "listBox_2save";
+            this.listBox_2save.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox_2save.Size = new System.Drawing.Size(235, 197);
+            this.listBox_2save.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.button_c2s);
+            this.flowLayoutPanel1.Controls.Add(this.button_s2c);
+            this.flowLayoutPanel1.Controls.Add(this.button_swap);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(249, 72);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(34, 78);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button_c2s
+            // 
+            this.button_c2s.Location = new System.Drawing.Point(3, 3);
+            this.button_c2s.Name = "button_c2s";
+            this.button_c2s.Size = new System.Drawing.Size(30, 20);
+            this.button_c2s.TabIndex = 0;
+            this.button_c2s.Text = ">";
+            this.button_c2s.UseVisualStyleBackColor = true;
+            this.button_c2s.Click += new System.EventHandler(this.button_c2s_Click);
+            // 
+            // button_s2c
+            // 
+            this.button_s2c.Location = new System.Drawing.Point(3, 29);
+            this.button_s2c.Name = "button_s2c";
+            this.button_s2c.Size = new System.Drawing.Size(30, 20);
+            this.button_s2c.TabIndex = 1;
+            this.button_s2c.Text = "<";
+            this.button_s2c.UseVisualStyleBackColor = true;
+            this.button_s2c.Click += new System.EventHandler(this.button_s2c_Click);
+            // 
+            // button_swap
+            // 
+            this.button_swap.Location = new System.Drawing.Point(3, 55);
+            this.button_swap.Name = "button_swap";
+            this.button_swap.Size = new System.Drawing.Size(30, 20);
+            this.button_swap.TabIndex = 2;
+            this.button_swap.Text = "<>";
+            this.button_swap.UseVisualStyleBackColor = true;
+            this.button_swap.Click += new System.EventHandler(this.button_swap_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.Controls.Add(this.button_refresh);
+            this.flowLayoutPanel2.Controls.Add(this.button_clear);
+            this.flowLayoutPanel2.Controls.Add(this.button_backup);
+            this.flowLayoutPanel2.Controls.Add(this.button_open);
+            this.flowLayoutPanel2.Controls.Add(this.checkBox_mutiline);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 229);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(533, 29);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(3, 3);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(75, 23);
+            this.button_refresh.TabIndex = 0;
+            this.button_refresh.Text = "Refresh";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(84, 3);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 1;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_backup
+            // 
+            this.button_backup.Location = new System.Drawing.Point(165, 3);
+            this.button_backup.Name = "button_backup";
+            this.button_backup.Size = new System.Drawing.Size(75, 23);
+            this.button_backup.TabIndex = 2;
+            this.button_backup.Text = "Backup";
+            this.button_backup.UseVisualStyleBackColor = true;
+            this.button_backup.Click += new System.EventHandler(this.button_backup_Click);
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(246, 3);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(150, 23);
+            this.button_open.TabIndex = 3;
+            this.button_open.Text = "Open in Regedit.exe";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // checkBox_mutiline
+            // 
+            this.checkBox_mutiline.AutoSize = true;
+            this.checkBox_mutiline.Location = new System.Drawing.Point(402, 3);
+            this.checkBox_mutiline.Name = "checkBox_mutiline";
+            this.checkBox_mutiline.Size = new System.Drawing.Size(90, 16);
+            this.checkBox_mutiline.TabIndex = 4;
+            this.checkBox_mutiline.Text = "MultiColumn";
+            this.checkBox_mutiline.UseVisualStyleBackColor = true;
+            this.checkBox_mutiline.CheckedChanged += new System.EventHandler(this.checkBox_mutiline_CheckedChanged);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(539, 261);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 261);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.Text = "Network Profile Cleaner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox_2clean;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox_2save;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button_c2s;
+        private System.Windows.Forms.Button button_s2c;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_backup;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button_swap;
+        private System.Windows.Forms.CheckBox checkBox_mutiline;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace NetworkProfilesCleaner
+{
+    public partial class Form1 : Form
+    {
+        public RegistryView regView;
+        public string systemBits;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_refresh_Click(object sender, EventArgs e)
+        {
+            button_refresh.Enabled = false;
+
+            // 打开注册表项
+            var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, regView)
+                .OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles");
+
+            if (key == null)
+            {
+                MessageBox.Show("Error when opening key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            listBox_2clean.Items.Clear();
+            listBox_2save.Items.Clear();
+
+            // 异步读取注册表
+            await Task.Run(() =>
+            {
+                foreach (string subKeyName in key.GetSubKeyNames())
+                {
+                    profile p = new profile(key.OpenSubKey(subKeyName));
+
+                    this.Invoke(new Action(() => {
+                        listBox_2clean.Items.Add(p);
+                    }));
+                }
+
+            });
+
+            button_refresh.Enabled = true;
+        }
+
+        private void button_c2s_Click(object sender, EventArgs e)
+        {
+            if (listBox_2clean.SelectedItems.Count == 0) { return; }
+            Utils.swapListBoxSelectedItems(listBox_2clean, listBox_2save);
+        }
+
+        private void button_s2c_Click(object sender, EventArgs e)
+        {
+            if (listBox_2save.SelectedItems.Count == 0) { return; }
+            Utils.swapListBoxSelectedItems(listBox_2save, listBox_2clean);
+        }
+
+        private void button_swap_Click(object sender, EventArgs e)
+        {
+            Utils.swapListBoxItems(listBox_2clean, listBox_2save);
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            if (listBox_2clean.Items.Count == 0
+                || MessageBox.Show(
+                    "Are you going to deleted all the network profiles in the Clear list?", 
+                    "Warnning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning
+                    ) != DialogResult.Yes)
+                { return; }
+
+            // 打开注册表项
+            var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, regView)
+                .OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles", true);
+
+            var exceptions = new List<Exception>();
+            var profiles2clean = Utils.extractItems(listBox_2clean.Items).Cast<profile>();
+            foreach (var p in profiles2clean)
+            {
+                try
+                {
+                    rootKey.DeleteSubKeyTree(p.GUID);
+                    listBox_2clean.Items.Remove(p);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+                
+            }
+
+            if (exceptions.Count == 0)
+            {
+                MessageBox.Show("Cleaned all!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(string.Join("\n", exceptions), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button_backup_Click(object sender, EventArgs e)
+        {
+            if (listBox_2clean.Items.Count == 0) { return; }
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Registry File|*.reg";
+            saveFileDialog1.Title = "Save profiles backup";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName == "") { return; }
+
+            Utils.ExportRegistryKeyArray(
+                listBox_2clean.Items.Cast<profile>().Select(
+                    profile => "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles\\" + profile.GUID),
+                saveFileDialog1.FileName
+            );
+
+            MessageBox.Show("Backuped all profiles in Clean list!", "Backup", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            Process.Start("reg.exe",
+                @"ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "
+                + "\""
+                + @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles"
+                + "\""
+                + " /f /reg:" + systemBits).WaitForExit();
+            Process.Start("C:\\Windows\\regedit", "-m");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+
+            // 32位进程只能打开32位regedit.exe，32位和64位有些注册表是隔离的
+            button_open.Enabled = !(Environment.Is64BitOperatingSystem ^ Environment.Is64BitProcess);
+
+            // 32Bits / 64Bits 注册表兼容
+            if (Environment.Is64BitOperatingSystem)
+            {
+                regView = RegistryView.Registry64;
+                systemBits = "64";
+            }
+            else
+            {
+                regView = RegistryView.Registry32;
+                systemBits = "32";
+            }
+        }
+
+        private void checkBox_mutiline_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox_2clean.MultiColumn = checkBox_mutiline.Checked;
+            listBox_2save.MultiColumn = checkBox_mutiline.Checked;
+        }
+    }
+
+
+    public static class Utils
+    {
+        public static object[] extractItems(ICollection items)
+        {
+            object[] temp = new object[items.Count];
+            items.CopyTo(temp, 0);
+            return temp;
+        }
+
+        /// <summary>
+        /// 交换两个 ListBox 控件的表项
+        /// </summary>
+        public static void swapListBoxItems(ListBox from, ListBox to)
+        {
+            var temp = extractItems(from.Items);
+
+            from.Items.Clear();
+            from.Items.AddRange(to.Items);
+            to.Items.Clear();
+            to.Items.AddRange(temp);
+
+        }
+
+        /// <summary>
+        /// 传送两个 ListBox 控件的已选中表项
+        /// </summary>
+        public static void swapListBoxSelectedItems(ListBox from, ListBox to)
+        {
+            var temp = extractItems(from.SelectedItems);
+
+            foreach (var item in temp)
+            {
+                from.Items.Remove(item);
+                to.Items.Add(item);
+            }
+
+        }
+
+        /// <summary>
+        /// 导出列表中的每一个注册表项，并合并为一个reg文件并导出
+        /// </summary>
+        /// <param name="registryKeys">注册表项的完整路径</param>
+        /// <param name="outputFile">输出文件名</param>
+        public static void ExportRegistryKeyArray(IEnumerable<string> registryKeys, string outputFile)
+        {
+            var systemBits = "64";
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                systemBits = "64";
+            }
+            else
+            {
+                systemBits = "32";
+            }
+
+
+            // 为每个注册表项创建一个临时.reg文件
+            var tempFiles = new List<string>();
+            var tasks = new List<Task>();
+            int i = 0;
+            foreach (var keyPath in registryKeys)
+            {
+                var tempFile = Path.GetTempFileName() + ".reg";
+                var task = Task.Run(() =>
+                {
+                    var arg = $"EXPORT \"{keyPath}\" \"{tempFile}\" /y /reg:{systemBits}";
+                    Debug.Print(arg);
+                    var startInfo = new ProcessStartInfo("reg.exe", arg)
+                    {
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+                    var process = new Process { StartInfo = startInfo };
+                    process.Start();
+                    process.WaitForExit();
+
+                    return process.StandardError.ReadToEnd() + process.StandardOutput.ReadToEnd(); 
+                });
+
+
+                tasks.Add(task);
+                tempFiles.Add(tempFile);
+                i++;
+            }
+
+            // 等待所有任务完成
+            Task.WaitAll(tasks.ToArray());
+
+            // 写入.reg文件的头部信息
+            //File.WriteAllText(outputFile, "Windows Registry Editor Version 5.00\n\n");
+
+            // 将所有临时.reg文件合并到新的.reg文件
+
+            foreach (string tempFile in tempFiles)
+            {
+                File.AppendAllText(outputFile, File.ReadAllText(tempFile) + "\n");
+                File.Delete(tempFile);  // 删除临时文件
+            }
+        }
+
+    }
+
+
+    internal struct profile
+    {
+        public object Name;
+        public string GUID;
+        public RegistryKey key;
+
+        public profile(RegistryKey key)
+        {
+            this.key = key;
+            this.Name = key.GetValue("ProfileName");
+            this.GUID = Path.GetFileName(key.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is profile other &&
+                   GUID == other.GUID;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1496550612;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(GUID);
+            return hashCode;
+        }
+
+        public void Deconstruct(out object name, out string guid)
+        {
+            name = Name;
+            guid = this.GUID;
+        }
+
+        public override string ToString()
+        {
+            return this.Name as string;
+        }
+    }
+}
